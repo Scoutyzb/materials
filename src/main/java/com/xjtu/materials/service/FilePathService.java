88d021100a78ba5afdb9dc3029f0d4dcf4c83d71
helong <44635029+helong920@users.noreply.github.com>
2019-01-22 20:49:26 +0800

    贺龙 bug

--- src/main/java/com/xjtu/materials/service/FilePathService.java
+++ src/main/java/com/xjtu/materials/service/FilePathService.java
@@ -9,7 +9,7 @@ public interface FilePathService {
     String Upload(MultipartFile file,String name,String username);
 
     // 未审核cif材料
-    List<UpLoadMaterial> unAuthMaterial();
+    List<UpLoadMaterial> getByIsAuthMaterial(String isAuthMaterial);
 
     void UploadPublication(String fileName,String authorName,String username,String abstractText,String DIO,String type,String adress);
 }
